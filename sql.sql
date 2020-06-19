cf931ba9516b287ba034e44d1cb3b01ec1fcb2af
Daniel Cho <dh.cho428@gmail.com>
2020-06-19 09:10:10 -0700

    Update sql.sql

--- sql.sql
+++ sql.sql
@@ -1,1 +1,1 @@
-create table calendar (thedate varchar(15),thecontent text);
+create table calendar (content2 text,content3 text,content4  text, content5   text,  c1 int , c2 int , c3 int , c4 int, c5 int ,thedate varchar(15),thecontent text);
