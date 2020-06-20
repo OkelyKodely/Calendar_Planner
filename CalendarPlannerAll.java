605520878a6b2c7ddd472a23976fe0e7c611e69e
Daniel Cho <dh.cho428@gmail.com>
2020-06-20 00:05:03 -0700

    Add files via upload

--- CalendarPlannerAll.java
+++ CalendarPlannerAll.java
@@ -1,3 +1,4 @@
+
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
@@ -6,6 +7,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -19,8 +21,12 @@ import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
+import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
+
+
 
 public class CalendarPlannerAll implements MouseListener {
 
@@ -42,7 +48,7 @@ public class CalendarPlannerAll implements MouseListener {
 
     /********
      * ***
-     * CONSt
+     * CONSTRUCTOR
      */
     public CalendarPlannerAll() {
 
@@ -69,11 +75,12 @@ public class CalendarPlannerAll implements MouseListener {
             }
             
             int thex = e.getX(); int they = e.getY() + 70;
-            
+            //user click on date
             if(thex >= x+100 && thex <= x+100 + 80 && they >= y+200 && they <= y+200 + 80) {
                 plan = new JFrame();
                 plan.setLayout(null);
                 plan2= new JPanel();
+                plan2.setBackground(Color.RED);
                 plan2.setLayout(null);
                 plan. setBounds(0, 0, 530, 950);
                 plan2.setBounds(plan.getBounds());
@@ -95,40 +102,100 @@ public class CalendarPlannerAll implements MouseListener {
                 JComboBox c1 = new JComboBox(new Object[]{"Unimportant", "Important", "Urgent"});
                 c1.setBounds(10, 50, 200, 20);
                 plan2.add(c1);
+                JLabel cl1 = new JLabel("Time");
+                cl1.setBounds(220, 50, 50, 20);
+                plan2.add(cl1);
+                JTextField cl1t = new JTextField();
+                cl1t.setBounds(330-50, 50, 100, 20);
+                plan2.add(cl1t);
                 JComboBox c2 = new JComboBox(new Object[]{"Unimportant", "Important", "Urgent"});
                 c2.setBounds(10, 230, 200, 20);
                 plan2.add(c2);
+                JLabel cl2 = new JLabel("Time");
+                cl2.setBounds(220, 230, 50, 20);
+                plan2.add(cl2);
+                JTextField cl2t = new JTextField();
+                cl2t.setBounds(330-50, 230, 100, 20);
+                plan2.add(cl2t);
                 JComboBox c3 = new JComboBox(new Object[]{"Unimportant", "Important", "Urgent"});
                 c3.setBounds(10, 400, 200, 20);
                 plan2.add(c3);
+                JLabel cl3 = new JLabel("Time");
+                cl3.setBounds(220, 400, 50, 20);
+                plan2.add(cl3);
+                JTextField cl3t = new JTextField();
+                cl3t.setBounds(330-50, 400, 100, 20);
+                plan2.add(cl3t);
                 JComboBox c4 = new JComboBox(new Object[]{"Unimportant", "Important", "Urgent"});
                 c4.setBounds(10, 540, 200, 20);
                 plan2.add(c4);
+                JLabel cl4 = new JLabel("Time");
+                cl4.setBounds(220, 540, 50, 20);
+                plan2.add(cl4);
+                JTextField cl4t = new JTextField();
+                cl4t.setBounds(330-50, 540, 100, 20);
+                plan2.add(cl4t);
                 JComboBox c5 = new JComboBox(new Object[]{"Unimportant", "Important", "Urgent"});
                 c5.setBounds(10, 720, 200, 20);
                 plan2.add(c5);
+                JLabel cl5 = new JLabel("Time");
+                cl5.setBounds(220, 720, 50, 20);
+                plan2.add(cl5);
+                JTextField cl5t = new JTextField();
+                cl5t.setBounds(330-50, 720, 100, 20);
+                plan2.add(cl5t);
                 
-                JTextArea jta = new JTextArea();
-                jta.setBounds(10, 100, 500, 100);
-                plan2.add(jta);
-                JTextArea jta2 = new JTextArea();
-                jta2.setBounds(10, 260, 500, 100);
-                plan2.add(jta2);
-                JTextArea jta3 = new JTextArea();
-                jta3.setBounds(10, 420, 500, 100);
-                plan2.add(jta3);
-                JTextArea jta4 = new JTextArea();
-                jta4.setBounds(10, 580, 500, 100);
-                plan2.add(jta4);
-                JTextArea jta5 = new JTextArea();
-                jta5.setBounds(10, 740, 500, 100);
-                plan2.add(jta5);
-                JButton b = new JButton("Update");
+                JButton b = new JButton("Save");
                 b.setBounds(10, 850, 200, 40);
                 plan2.add(b);
+
+                JButton cancelBttn = new JButton("Erase");
+                cancelBttn.setBounds(240, 850, 200, 40);
+                plan2.add(cancelBttn);
+                
+                JTextArea jta = new JTextArea();
+                jta.setLineWrap(true);
+                jta.setWrapStyleWord(true);
+                JScrollPane jta_ = new JScrollPane(jta,
+            JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
+            JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+                jta_.setBounds(10, 100, 500, 100);
+                plan2.add(jta_);
+                JTextArea jta2 = new JTextArea();
+                jta2.setLineWrap(true);
+                jta2.setWrapStyleWord(true);
+                JScrollPane jta2_ = new JScrollPane(jta2,
+            JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
+            JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+                jta2_.setBounds(10, 260, 500, 100);
+                plan2.add(jta2_);
+                JTextArea jta3 = new JTextArea();
+                jta3.setLineWrap(true);
+                jta3.setWrapStyleWord(true);
+                JScrollPane jta3_ = new JScrollPane(jta3,
+            JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
+            JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+                jta3_.setBounds(10, 420, 500, 100);
+                plan2.add(jta3_);
+                JTextArea jta4 = new JTextArea();
+                jta4.setLineWrap(true);
+                jta4.setWrapStyleWord(true);
+                JScrollPane jta4_ = new JScrollPane(jta4,
+            JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
+            JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+                jta4_.setBounds(10, 580, 500, 100);
+                plan2.add(jta4_);
+                JTextArea jta5 = new JTextArea();
+                jta5.setLineWrap(true);
+                jta5.setWrapStyleWord(true);
+                JScrollPane jta5_ = new JScrollPane(jta5,
+            JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
+            JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+                jta5_.setBounds(10, 740, 500, 100);
+                plan2.add(jta5_);
                 
                 final int thei = i+1;
-                
+                        
                 try {
                     
                     statement = connection.createStatement();
@@ -138,6 +205,11 @@ public class CalendarPlannerAll implements MouseListener {
                     ResultSet rs = statement.executeQuery(sqlString);
                     
                     if(rs.next()) {
+                        cl1t.setText(rs.getString("c1t"));
+                        cl2t.setText(rs.getString("c2t"));
+                        cl3t.setText(rs.getString("c3t"));
+                        cl4t.setText(rs.getString("c4t"));
+                        cl5t.setText(rs.getString("c5t"));
                         jta.setText(rs.getString("thecontent"));
                         jta2.setText(rs.getString("content2"));
                         jta3.setText(rs.getString("content3"));
@@ -226,13 +298,14 @@ public class CalendarPlannerAll implements MouseListener {
                             if(c5.getSelectedIndex() == 2)
                                 c_5 = 3;
                             
-                            String sqlString = "update calendar set thecontent = '"+jta.getText()+"', content2 = '"+jta2.getText()+"', content3 = '"+jta3.getText()+"', content4 = '"+jta4.getText()+"', content5 = '"+jta5.getText()+"', c1 = "+c_1+", c2 = "+c_2+", c3 = "+c_3+", c4 = "+c_4+", c5 = "+c_5+" where thedate = '"+month + "/" + thei + "/" + year+"';";
+                            String sqlString = "update calendar set thecontent = '"+jta.getText()+"', content2 = '"+jta2.getText()+"', content3 = '"+jta3.getText()+"', content4 = '"+jta4.getText()+"', content5 = '"+jta5.getText()+"', c1 = "+c_1+", c2 = "+c_2+", c3 = "+c_3+", c4 = "+c_4+", c5 = "+c_5+", c1t = '"+cl1t.getText()+"', c2t = '"+cl2t.getText()+"', c3t = '"+cl3t.getText()+"', c4t = '"+cl4t.getText()+"', c5t = '"+cl5t.getText()+"' where thedate = '"+month + "/" + thei + "/" + year+"';";
                             
                             if(pleaseInsertMe) {
-                                sqlString = "insert into calendar (c1, c2, c3, c4, c5, thedate, thecontent, content2, content3, content4, content5) values ("+c_1+","+c_2+","+c_3+","+c_4+","+c_5+",'"+month + "/" + thei + "/" + year+"','"+jta.getText()+"','"+jta2.getText()+"','"+jta3.getText()+"','"+jta4.getText()+"','"+jta5.getText()+"');";
+                                sqlString = "insert into calendar (c1t, c2t, c3t, c4t, c5t, c1, c2, c3, c4, c5, thedate, thecontent, content2, content3, content4, content5) values ('"+cl1t.getText()+"','"+cl2t.getText()+"','"+cl3t.getText()+"','"+cl4t.getText()+"','"+cl5t.getText()+"',"+c_1+","+c_2+","+c_3+","+c_4+","+c_5+",'"+month + "/" + thei + "/" + year+"','"+jta.getText()+"','"+jta2.getText()+"','"+jta3.getText()+"','"+jta4.getText()+"','"+jta5.getText()+"');";
                             }
                             
                             statement.execute(sqlString);
+                            statement.close();
                             
                             java.util.Calendar c = java.util.Calendar.getInstance();
                             c.setTime(new Date(year - 1900, month - 1, 1));
@@ -246,6 +319,33 @@ public class CalendarPlannerAll implements MouseListener {
                         }
                         
                         plan.dispose();
+                    }
+
+                });
+
+                cancelBttn.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        try {
+                            
+                            statement = connection.createStatement();
+                            
+                            String sqlString = "delete from calendar where thedate = '"+month + "/" + thei + "/" + year+"';";
+                            
+                            statement.execute(sqlString);
+                            statement.close();
+
+                            plan.dispose();
+                            
+                            java.util.Calendar c = java.util.Calendar.getInstance();
+                            c.setTime(new Date(year - 1900, month - 1, 1));
+                            int dayOfWeek = c.get(java.util.Calendar.DAY_OF_WEEK);
+                            YearMonth yearMonthObject = YearMonth.of((year), month);
+                            int daysInMonth = yearMonthObject.lengthOfMonth();
+                            displayDayz(daysInMonth, dayOfWeek);
+                        } catch(SQLException sqle) {
+                            sqle.printStackTrace();
+                        }
                     }
                 });
             }
@@ -287,26 +387,27 @@ public class CalendarPlannerAll implements MouseListener {
     public void setAllThings() {
         frame = new JFrame();
         panel = new JPanel();
+        panel.setBackground(Color.RED);
         start = true;
         frame.setTitle("Calendar Planner");
         frame.setLayout(null);
         frame.setBounds(0, 0, 1250, 760);
         panel.setBounds(frame.getBounds());
         frame.add(panel);
-        todayMonth = new JButton("Tonight");
+        todayMonth = new JButton("Current Month");
         previousMonth = new JButton("< Prev Month");
         nextMonth = new JButton("Next Month >");
         panel.add(todayMonth);
         panel.add(previousMonth);
         panel.add(nextMonth);
-        todayMonth.setBounds(11, 50, 100, 20);
+        todayMonth.setBounds(11, 50, 200, 20);
         todayMonth.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 Thread t = new Thread() {
                     public void run() {
                         try {
-                            graphics.setColor(Color.white);
+                            graphics.setColor(Color.MAGENTA);
                             graphics.fillRect(0, 50, 1250, 760);
                             Image image = ImageIO.read(getClass().getResourceAsStream("background.jpg"));
                             graphics.drawImage(image, 650, 50, image.getWidth(null), image.getHeight(null), null);
@@ -322,7 +423,7 @@ public class CalendarPlannerAll implements MouseListener {
                         YearMonth yearMonthObject = YearMonth.of((year), month);
                         int daysInMonth = yearMonthObject.lengthOfMonth();
                         graphics.setColor(Color.BLACK);
-                        graphics.drawString(month + "-" + day + "-" + (year), 100, 100);
+                        graphics.drawString(month + "-" + (year), 100, 100);
                         displayDayz(daysInMonth, dayOfWeek);
                     }
                 };
@@ -337,7 +438,7 @@ public class CalendarPlannerAll implements MouseListener {
                 Thread t = new Thread() {
                     public void run() {
                         try {
-                            graphics.setColor(Color.white);
+                            graphics.setColor(Color.MAGENTA);
                             graphics.fillRect(0, 50, 1250, 760);
                             Image image = ImageIO.read(getClass().getResourceAsStream("background.jpg"));
                             graphics.drawImage(image, 650, 50, image.getWidth(null), image.getHeight(null), null);
@@ -359,7 +460,7 @@ public class CalendarPlannerAll implements MouseListener {
                         YearMonth yearMonthObject = YearMonth.of((year), month);
                         int daysInMonth = yearMonthObject.lengthOfMonth();
                         graphics.setColor(Color.BLACK);
-                        graphics.drawString(month + "-" + day + "-" + (year), 100, 100);
+                        graphics.drawString(month + "-" + (year), 100, 100);
                         displayDayz(daysInMonth, dayOfWeek);
                     }
                 };
@@ -374,7 +475,7 @@ public class CalendarPlannerAll implements MouseListener {
                 Thread t = new Thread() {
                     public void run() {
                         try {
-                            graphics.setColor(Color.white);
+                            graphics.setColor(Color.MAGENTA);
                             graphics.fillRect(0, 50, 1250, 760);
                             Image image = ImageIO.read(getClass().getResourceAsStream("background.jpg"));
                             graphics.drawImage(image, 650, 50, image.getWidth(null), image.getHeight(null), null);
@@ -396,7 +497,7 @@ public class CalendarPlannerAll implements MouseListener {
                         YearMonth yearMonthObject = YearMonth.of((year), month);
                         int daysInMonth = yearMonthObject.lengthOfMonth();
                         graphics.setColor(Color.BLACK);
-                        graphics.drawString(month + "-" + day + "-" + (year), 100, 100);
+                        graphics.drawString(month + "-" + (year), 100, 100);
                         displayDayz(daysInMonth, dayOfWeek);
                     }
                 };
@@ -455,8 +556,8 @@ public class CalendarPlannerAll implements MouseListener {
             @Override
             public void run() {
                 
-                graphics.setColor(Color.gray);
-                graphics.drawString("SUN     MON     TUES      WED     THUR     FRI    SAT",100,145);
+                graphics.setColor(Color.GREEN);
+                graphics.drawString("SUN     MON     TUES      WED     THUR     FRI      SAT", 100, 145);
 
                 int x=0;
                 int y=0;
@@ -481,6 +582,9 @@ public class CalendarPlannerAll implements MouseListener {
                             graphics.setColor(Color.GREEN);
                             graphics.drawString("---",100+x+10-12,y+200+40-20);
                         }
+                        
+                        rs.close();
+                        statement.close();
 
                     } catch(SQLException sqle) {
                         sqle.printStackTrace();
@@ -496,8 +600,14 @@ public class CalendarPlannerAll implements MouseListener {
                         graphics.drawOval(100+x-25,y+200-42,70,70);
                     }
 
-                    graphics.setColor(Color.BLACK);
-                    graphics.drawString(String.valueOf(i+1),100+x,y+200);
+                    if(x == 0) {
+                        graphics.setColor(Color.GREEN);
+                        graphics.drawString(String.valueOf(i+1),100+x,y+200);
+                    }
+                    else {
+                        graphics.setColor(Color.BLACK);
+                        graphics.drawString(String.valueOf(i+1),100+x,y+200);
+                    }
                 }
             }
         });
